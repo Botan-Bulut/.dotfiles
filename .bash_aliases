d823231b982cc17bc65508c3a5a16a3527ef5cf8
Botan-Bulut <botan.datascience@gmail.com>
2024-10-11 13:18:29 +0300

    update.

--- .bash_aliases
+++ .bash_aliases
@@ -1,5 +1,5 @@
 alias safe_upgrade="sudo apt update && apt upgrade"
 alias configure_nvim="vim ~/.config/nvim/init.vim"
 alias configure_bash="vim ~/.bashrc"
-alias env_activate_ISL="source /home/botan/.python_venvs/ISL/bin/activate"
+alias env_activate_ISL="source /home/botan/.python_venv/ISL/bin/activate"
 alias ssh_ubuntu_server="ssh -p 2222 ubuntu@localhost"
--- links.sh
+++ links.sh
@@ -1,3 +1,9 @@
-ln -s /home/$USER/.dotfiles/nvim /home/$USER/.config/
-ln -s /home/$USER/.dotfiles/.bashrc /home/$USER/.bashrc
-ln -s /home/$USER/.dotfiles/.bash_aliases /home/$USER/.bash_aliases
+sudo rm -rf /home/$USER/.config/nvim
+sudo rm -rf /home/$USER/.bashrc
+sudo rm -rf /home/$USER/.bash_aliases
+sudo rm -rf /root/.config/nvim
+
+sudo ln -s /home/$USER/.dotfiles/nvim /home/$USER/.config/
+sudo ln -s /home/$USER/.dotfiles/nvim /root/.config/
+sudo ln -s /home/$USER/.dotfiles/.bashrc /home/$USER/.bashrc
+sudo ln -s /home/$USER/.dotfiles/.bash_aliases /home/$USER/.bash_aliases
