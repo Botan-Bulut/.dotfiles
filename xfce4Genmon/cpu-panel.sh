732ebd5ad7851fec39278b9cbc63a448f0988120
Botan Bulut <81382661+Botan-Bulut@users.noreply.github.com>
2026-01-26 01:36:05 +0300

    Update cpu-panel.sh

--- xfce4Genmon/cpu-panel.sh
+++ xfce4Genmon/cpu-panel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 readonly CPU_PERCENTAGE=$(top -b -n1 | grep "%Cpu" | awk '{sum=(100 - $8)} END {print int(sum)}')
+readonly CPU_TEMP=$(sensors | awk '/CPU:/ {gsub(/\+|°C/,"",$2); print $2}')
 INFO="<txt>"
-INFO+=" CPU  ${CPU_PERCENTAGE}% "
+INFO+=" CPU  ${CPU_PERCENTAGE}% - ${CPU_TEMP}°C"
 INFO+="</txt>"
 echo -e "${INFO}"
-
