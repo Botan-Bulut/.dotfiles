829c7f5f5591d453f5f8d975cf5a76cd43c96f20
Botan Bulut <81382661+Botan-Bulut@users.noreply.github.com>
2026-01-27 14:56:18 +0300

    Update memory-panel.sh

--- xfce4Genmon/memory-panel.sh
+++ xfce4Genmon/memory-panel.sh
@@ -2,7 +2,7 @@
 # Dependencies: bash>=3.2, coreutils, file, gawk, grep, lm_sensors, sed, xfce4-taskmanager
 
 # Get RAM usage percentage
-readonly USED_RAM_MB=$(free --si -t -h | grep Total | awk '{gsub("G", "", $3); print $3}')
+readonly USED_RAM_MB=$(free --si -h | awk '/^Mem:/ {gsub("G","",$3); print $3}')
 
 INFO="<txt>"
 INFO+=" MEM ${USED_RAM_MB}G "
